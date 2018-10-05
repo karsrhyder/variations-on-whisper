e203c385348c9ce6044e3751c483466ec12949a9
Kars Rhyder <karsrhyder@gmail.com>
2018-10-05 12:47:00 +0200

    basic encr/decr working

--- Implementation-0.js
+++ Implementation-0.js
@@ -1,9 +1,6 @@
 const Gossip = require('./common.js');
 const gossip = new Gossip();
-const btoa = require('btoa');
-const nacl = require('tweetnacl');
-const naclUtil = require('tweetnacl-util');
-nacl.util = naclUtil;
+
 
 let dappSym;
 let sender; 
@@ -15,9 +12,8 @@ const Web3 = require('web3');
 const url = 'ws://178.62.244.110:8546'
 const web3 = new Web3(url);
 
-gossip.on('message', (input) => {
-    console.log(`Received data: "${JSON.stringify(input)}"`);
-
+gossip.on('message', (msg) => {
+    console.log('Received data: ', msg);
 });
 
 gossip.init().then((res) => { 
@@ -29,29 +25,16 @@ async function start() {
     receiver = await gossip.createIdentity();
     thirdparty = await gossip.createIdentity();
     
-    setInterval(() => {
+    //setInterval(() => {
         post();
-    }, 1000);
+    //}, 1000);
 };
 
 async function post() {
-    let dappSym = await gossip.getDappSym();
-    // Hash
-    const hash = web3.utils.sha3(web3.utils.sha3(sender.privateKey) + receiver.publicKey);
-    // Message String
-    const messageString = 'Hello world!';
-    // Encrypt
-    //const keyEncoded = btoa(nacl.randomBytes(10)); // this should be what?
-    const keyEncoded = 'KzpSTk1pezg5eTJRNmhWJmoxdFo6UDk2WlhaOyQ5N0U='
-    const nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
-    const key = nacl.util.decodeBase64(keyEncoded);
-    const messageBytes = nacl.util.decodeUTF8(messageString);
-    const box = nacl.secretbox(messageBytes, nonce, key);
-    const nonceEncoded = nacl.util.encodeBase64(nonce);
-    const cypherText = nacl.util.encodeBase64(box);
-    // Concat payload
-    const payload = `${hash}.${nonceEncoded}.${cypherText}`;
-    return (gossip.postMessage(dappSym, payload, receiver));
+    let from = sender;
+    let to = receiver;
+    let payload = 'Hello world';
+    return (gossip.postMessage(payload, from, to));
     //return (true);
 };
 
--- common.js
+++ common.js
@@ -2,6 +2,9 @@ const EventEmitter = require('events');
 const Web3 = require('web3');
 let web3;
 let dappSym;
+const nacl = require('tweetnacl');
+const naclUtil = require('tweetnacl-util');
+nacl.util = naclUtil;
 
 class Gossip extends EventEmitter {
     constructor() {
@@ -14,6 +17,7 @@ class Gossip extends EventEmitter {
         dappSym = await web3.shh.generateSymKeyFromPassword('swarmcity1');
         let Id = await web3.shh.addSymKey('0x'+dappSym);
         let hasit = await web3.shh.hasSymKey(dappSym);
+        console.log('init: ', dappSym, Id, hasit);
         return dappSym;
     };
 
@@ -31,7 +35,21 @@ class Gossip extends EventEmitter {
                 //console.log('listening on ', _dappSymKeyId, ' - ', Date.now());
                 if (!msgs.length) return 
                 msgs.forEach(m => {
-                    this.emit('message', m.payload, m.sig);
+                    //if (hash === hashOfInterest) {
+                        const decodePayload = web3.utils.hexToUtf8(m.payload);
+                        const [hash, nonceEncoded, cypherText] = decodePayload.split(".");
+                        //console.log('hash: ', hash, ' - nonceEncoded: ', nonceEncoded, ' - Cyphertext: ', cypherText);
+                        const keyEncoded = 'KzpSTk1pezg5eTJRNmhWJmoxdFo6UDk2WlhaOyQ5N0U=';
+                        const key = nacl.util.decodeBase64(keyEncoded);
+                        const nonce = nacl.util.decodeBase64(nonceEncoded);
+                        const box = nacl.util.decodeBase64(cypherText);
+
+                        // decrypt
+                        const messageBytes = nacl.secretbox.open(box, nonce, key);
+                        //console.log('hashofinterest: ', nacl.util.encodeUTF8(messageBytes));
+                        const msg = nacl.util.encodeUTF8(messageBytes);
+                    //}
+                        this.emit('message', msg, m.sig);
                 })
             }), 1000)
         });
@@ -54,19 +72,38 @@ class Gossip extends EventEmitter {
         return randomstring;
     };
 
-    async postMessage(_dappSymKeyId, _message, _from) {
+    async postMessage(_message, _to ,_from) {
         // whatever
         //console.log('posting: ', JSON.stringify(_message));
+        // Hash
+        let dappSymHere = this.getDappSym();
+
+        //console.log('post: ', _from.privateKey, _to.publicKey, _message, dappSym);
+        const hash = web3.utils.sha3(web3.utils.sha3(_from.privateKey) + _to.publicKey);
+        // Message String
+        const messageString = _message;
+        // Encrypt
+        //const keyEncoded = btoa(nacl.randomBytes(10)); // this should be what?
+        const keyEncoded = 'KzpSTk1pezg5eTJRNmhWJmoxdFo6UDk2WlhaOyQ5N0U='
+        const nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
+        const key = nacl.util.decodeBase64(keyEncoded);
+        const messageBytes = nacl.util.decodeUTF8(messageString);
+        const box = nacl.secretbox(messageBytes, nonce, key);
+        const nonceEncoded = nacl.util.encodeBase64(nonce);
+        const cypherText = nacl.util.encodeBase64(box);
+        // Concat payload
+        const payload = `${hash}.${nonceEncoded}.${cypherText}`;
+
         web3.shh.post({
-            symKeyID: _dappSymKeyId,
+            symKeyID: dappSymHere,
             ttl: 60,
             sig: _from.keyPair,
             powTarget: 2.01,
             powTime: 2,
             topic: '0x00000001',
-            payload: web3.utils.fromAscii(_message, + Date.now())
+            payload: web3.utils.fromAscii(payload)
         }).then(hash => {
-            console.log('Successfully posted message ', JSON.stringify(_message), ' --- ', hash, + Date.now())
+            //console.log('Successfully posted message ', JSON.stringify(payload), ' --- ', hash, + Date.now())
         }).catch(err => {
             console.error('Error posting msg: ',err)
         }); 
@@ -87,9 +124,7 @@ class Gossip extends EventEmitter {
         });
     };
 
-    onMessage(data) {
-        this.emit('data', data);
-    }
+
 }
 
 module.exports = Gossip
