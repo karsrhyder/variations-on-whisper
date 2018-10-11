e248a3021237aa64b3445a7084316307294711ee
Unknown <guillem.lion.benosa@gmail.com>
2018-10-11 03:06:06 +0200

    Support multiple msgs + create user classes

--- mailServerMock.js
+++ mailServerMock.js
@@ -2,6 +2,10 @@ const msg = require('./lib/msg')
 const sign = require('./lib/sign')
 const crypt = require('./lib/crypt')
 const EventEmitter = require('events');
+const colors = require('colors/safe');
+
+const colorsArray = ['magenta', 'yellow', 'cyan', 'green', 'red', 'blue']
+let counter = 0
 
 
 // ## 1. Use case
@@ -28,93 +32,140 @@ const EventEmitter = require('events');
 // });
 // myEmitter.emit('event', arg);
 
+const regularMsgTopic = '0x000001'
+const imOnlineMsgTopic = '0x000002'
+const topics = [regularMsgTopic, imOnlineMsgTopic]
+
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 
-const startTime = Date.now()
+// Logger:
+topics.forEach(topic => {
+    const startTime = Date.now()
+    myEmitter.on(topic, payload => {
+        const t = String(Date.now() - startTime).padStart(6)
+        const msgHash = msg.regular.hash(payload)
+        console.log(colors.gray(`LOG: ${t} ms ${topic}, payload hash: ${msgHash}`))
+    });
+})
 
 // Setup listeners
 // ===============
-const regularMsgTopic = '0x000001'
-const imOnlineMsgTopic = '0x000002'
-const chatPrivateKey = crypt.generateKey()
-const privateKeyB = '0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709'
-const addressB = sign.getAddress(privateKeyB)
-const timeToOnlineB = 2000
 
-// Logger:
-const t = () => String(Date.now() - startTime).padStart(6)
-myEmitter.on(regularMsgTopic, payload => {
-    console.log(`LOG event: ${t()}ms ${regularMsgTopic}, payload hash: ${msg.regular.hash(payload)}`)
-});
-myEmitter.on(imOnlineMsgTopic, payload => {
-    console.log(`LOG event: ${t()}ms ${imOnlineMsgTopic}, payload hash: ${msg.regular.hash(payload)}`)
-});
+const timeToOnlineB = 1000
 
-// C: Mailserver
-const msgs = {}
-const rebroadcastedMsgs = {}
-myEmitter
-.on(imOnlineMsgTopic, payload => {
-    // Ignore unauthorized messages
-    if (!msg.online.authorize(payload)) {
-      // Maybe punishable action could be taken against this user
-      return console.log(
-        "Unauthorized user attempted to access messages of hash: " + payload
-      );
-    }
-    const { hash } = msg.online.decode(payload);
-    // Search the database for matching msgs
-    for (const _hash of Object.keys(msgs)) {
-        if (_hash === hash) {
-            console.log('C: re-broadcasting message after imOnlineMsg')
-            rebroadcastedMsgs[msg.regular.hash(msgs[_hash])] = true
-            myEmitter.emit(regularMsgTopic, msgs[_hash]);
-            delete msgs[_hash]
-        }
-    }
-})
-.on(regularMsgTopic, payload => {
-    // Prevent listening to the msgs you have broadcasted
-    if (rebroadcastedMsgs[msg.regular.hash(payload)]) {
-        return
-    }
-    const { hash } = msg.regular.decode(payload);
-    console.log('C: stored msg')
-    msgs[hash] = payload
-});
+const userA = new User('A', sign.generateKey())
+const userB = new User('B', sign.generateKey())
+const mailServerC = new MailServer('C')
 
 
 // Setup Emitters
 // ==============
 
 // A: Emitter
-emitA()
-function emitA() {
-    const message = 'Super cool message from A to B'
-    const cipher = crypt.encrypt(message, chatPrivateKey);
-    const payload = msg.regular.encode(chatPrivateKey, addressB, cipher);
-    myEmitter.emit(regularMsgTopic, payload);
-}
+userB.ignore(true)
+
+const chatPrivateKey = crypt.generateKey()
+userA.join(chatPrivateKey)
+userB.join(chatPrivateKey)
+
+userA.message(userB.address, userA.chats()[0], 'Super cool message from A to B')
+userA.message(userB.address, userA.chats()[0], 'Another super cool message from A to B')
 
 // B: Receiver
+
 setTimeout(() => {
-    myEmitter.on(regularMsgTopic, payload => {
-        // Ignore unauthorized messages
-        const hashOfInterest = msg.regular.getHash(chatPrivateKey, addressB)
-        const { hash, cipher } = msg.regular.decode(payload);
-        if (hash === hashOfInterest) {
-            const message = crypt.decrypt(cipher, chatPrivateKey)
-            console.log('B: received a msg of interest: '+message)
-        } else {
-            // B is not storing msgs
-        }
-    });
-
-    const payload = msg.online.encode(chatPrivateKey, addressB, privateKeyB);
-    myEmitter.emit(imOnlineMsgTopic, payload);
-
+    userB.ignore(false)
+    userB.online()
+    setTimeout(() => {
+        userB.online()
+        userA.message(userB.address, userA.chats()[0], 'Last message from A to B')
+    }, 500)
 }, timeToOnlineB)
+
+
+
+// Define user classes
+// ===================
+
+function User(name, privateKey) {
+    const address = sign.getAddress(privateKey)
+    const chats = []
+    let ignore = false
+
+    const color = colorsArray[counter++]
+    const log = (txt) => console.log(colors[color](`--${name}: ${txt}`))
+
+    return {
+        ignore: (_ignore) => {
+            ignore = _ignore
+        },
+        online: () => {
+            log(`i'm online`)
+            const payload = msg.online.encode(chatPrivateKey, address, privateKey);
+            myEmitter.emit(imOnlineMsgTopic, payload);
+        },
+        join: (chatPrivateKey) => {
+            chats.push(chatPrivateKey)
+            myEmitter.on(regularMsgTopic, payload => {
+                if (ignore) return
+                // Ignore unauthorized messages
+                const hashOfInterest = msg.regular.getHash(chatPrivateKey, address)
+                const { hash, cipher } = msg.regular.decode(payload);
+                if (hash === hashOfInterest) {
+                    const message = crypt.decrypt(cipher, chatPrivateKey)
+                    log(`received a msg of interest: ${message}`)
+                }
+            });
+        },
+        message: (addressRecepient, chatPrivateKey, message) => {
+            log(`sent message: ${message}`)
+            const cipher = crypt.encrypt(message, chatPrivateKey);
+            const payload = msg.regular.encode(chatPrivateKey, addressRecepient, cipher);
+            myEmitter.emit(regularMsgTopic, payload);
+        },
+        chats: () => chats,
+        address
+    }
+}
+
+function MailServer(name) {
+    // C: Mailserver
+    const msgs = []
+    const rebroadcastedMsgs = {}
+
+    const color = colorsArray[counter++]
+    const log = (txt) => console.log(colors[color](`--${name}: ${txt}`))
+
+    myEmitter
+    .on(imOnlineMsgTopic, payload => {
+        // Ignore unauthorized messages
+        if (!msg.online.authorize(payload)) {
+        // Maybe punishable action could be taken against this user
+        return console.log(
+            "Unauthorized user attempted to access messages of hash: " + payload
+        );
+        }
+        const { hash } = msg.online.decode(payload);
+        // Search the database for matching msgs
+        if (msgs[hash]) {
+            log(`re-broadcasting message after imOnlineMsg`)
+            msgs[hash].forEach(msg => myEmitter.emit(regularMsgTopic, msg))
+            delete msgs[hash]
+        }
+    })
+    .on(regularMsgTopic, payload => {
+        // Prevent listening to the msgs you have broadcasted
+        const msgHash = msg.regular.hash(payload)
+        if (rebroadcastedMsgs[msgHash]) return
+        else rebroadcastedMsgs[msgHash] = true
+
+        const { hash } = msg.regular.decode(payload);
+        log(`stored msg`)
+        if (!msgs[hash]) msgs[hash] = []
+        msgs[hash].push(payload)
+    });
+}
 
 
 
